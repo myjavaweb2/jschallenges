d3c244d83b89038cbba623bb6a885016a1b3996b
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-09-21 16:47:05 -0400

    adding challenge 2 alternate

--- challenge_2_alt/challenge2_alt.js
+++ challenge_2_alt/challenge2_alt.js
@@ -30,6 +30,84 @@
 
  $(function(){
 
+ 	// The code below answers #1 question for when teamone shots.
+ 	// The code below also answers #3 question in the "if statement" to change the background color of the body each time a team hits.
+ 	var goal = false;
+	$( "#teamone-shoot" ).click(function( event ) {
 
+		var teamoneShotsTaken = $("#teamone-numshots").text();
+		teamoneShotsTaken++
+		$("#teamone-numshots").text(teamoneShotsTaken);
+
+		var teamoneCurrentGoals = $("#teamone-numhits").text();
+		
+		var teamPlayShoot =  Math.round(Math.random() * (100 - 0) + 0);
+		
+		if (teamPlayShoot % 2 == 0) {
+			goal = true;
+			teamoneCurrentGoals++
+			$("#teamone-numhits").text(teamoneCurrentGoals);
+
+		  $('body').css('background-color', 'red');
+
+		  new Audio("sounds/Pew_Pew.mp3").play();
+			
+		} else if (teamPlayShoot % 2 == 1 ) {
+			goal = false;
+			$('body').css('background-color', 'white');
+		}
+	});
+
+	// The code below also answers #1 question for when teamtwo shots.
+	$( "#teamtwo-shoot" ).click(function( event ) {
+
+		var teamtwoShotsTaken = $("#teamtwo-numshots").text();
+		teamtwoShotsTaken++
+		$("#teamtwo-numshots").text(teamtwoShotsTaken);
+
+		var teamtwoCurrentGoals = $("#teamtwo-numhits").text();
+
+		var teamPlayShoot =  Math.round(Math.random() * (100 - 0) + 0);
+		
+		if (teamPlayShoot % 2 == 0) {
+			goal = true;
+			teamtwoCurrentGoals++
+			$("#teamtwo-numhits").text(teamtwoCurrentGoals);
+
+			$('body').css('background-color', 'blue');
+
+			new Audio("sounds/bizarre_guitar.mp3").play();
+
+		} else if (teamPlayShoot % 2 == 1 ) {
+			goal = false;
+			$('body').css('background-color', 'white');
+		}	
+	});
+
+
+	// The code below answers #2 question to reset the number of shots and scores for both teams and increase the reset count by 1.
+	$( "#reset" ).click(function( event ) {
+
+		var currentNumberOfResets = $("#num-resets").text();
+		currentNumberOfResets++
+		$("#num-resets").text(currentNumberOfResets);
+
+		var resetTeamoneShots = $("#teamone-numshots").text();
+		resetTeamoneShots = 0;
+		$("#teamone-numshots").text(resetTeamoneShots);
+
+		var resetTeamoneHits = $("#teamone-numhits").text();
+		resetTeamoneHits = 0;
+		$("#teamone-numhits").text(resetTeamoneHits);
+
+		var resetTeamtwoShots = $("#teamtwo-numshots").text();
+		resetTeamtwoShots = 0;
+		$("#teamtwo-numshots").text(resetTeamtwoShots);
+
+		var resetTeamoneHits = $("#teamtwo-numhits").text();
+		resetTeamtwoHits = 0;
+		$("#teamtwo-numhits").text(resetTeamtwoHits);
+		
+	});
 
  })
